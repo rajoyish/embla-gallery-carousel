97af5ffb4cdae84cee09ad26072383732e6488a5
Rajesh Budhathoki <rajoyish@gmail.com>
2024-06-19 23:26:01 +0545

    Autoplay & WIP Heading

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import EmblaCarousel from "embla-carousel";
+import Autoplay from "embla-carousel-autoplay";
 import { setupTweenOpacity } from "./EmblaCarouselTweenOpacity";
 import { addDotBtnsAndClickHandlers } from "./EmblaCarouselDotButton";
 import { addPrevNextBtnsClickHandlers } from "./EmblaCarouselArrowButtons";
@@ -13,7 +14,14 @@ const prevBtn = emblaNode.querySelector(".embla__button--prev");
 const nextBtn = emblaNode.querySelector(".embla__button--next");
 const dotsNode = document.querySelector(".embla__dots");
 
-const emblaApi = EmblaCarousel(viewportNode, OPTIONS);
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [
+  Autoplay({
+    playOnInit: true,
+    delay: 5000,
+    stopOnMouseEnter: true,
+    stopOnInteraction: false,
+  }),
+]);
 const removeTweenOpacity = setupTweenOpacity(emblaApi);
 
 const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
